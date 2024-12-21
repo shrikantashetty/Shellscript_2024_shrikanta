d126af34fe92a44eb56e0c88b637f7c6201a7e16
shrikantashetty <shettyshrikanta@gmail.com>
2024-12-21 20:01:31 +0530

    Test Merge Test

--- addset_num.sh
+++ addset_num.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 arr_name="10 20 2 30 50"
-sum=0
+sum1=0
 for i in $arr_name
 do
-	sum=`expr $sum + $i`
+	sum=`expr $sum1 + $i`
 done
-echo "sum of array is $sum"
+echo "sum of array is $sum1"
